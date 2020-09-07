8e9951ed9e991e2b2f9baea976b0d2e4f2be5e63
Caitlin White <62992911+caitlinritawhite@users.noreply.github.com>
2020-09-07 16:28:47 -0400

    Update mainHTML.js

--- Develop/templates/mainHTML.js
+++ Develop/templates/mainHTML.js
@@ -12,6 +12,23 @@ function mainHTML(data) {
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
+          <style>
+            .jumbotron {
+                background-color: #203354;
+            }
+            body {
+                background-color: white;
+            }
+            h1 {
+                color: white;
+            }
+            .card-header, .card-body {
+                background-color: white;
+            }
+            .fas {
+                color: #203354
+            }
+        </style>
 </head>
 
 <body>
